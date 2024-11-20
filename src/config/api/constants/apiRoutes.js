1bfd7d973c9d7068800a9f59ded6f381234c4f6a
Emily Menchu <menchu.lucila@gmail.com>
2024-11-20 13:13:30 -0600

    [ADD] SE OPTIMIZO LA LOGICA DEL SIDEMENU UTILIZANDO MODULOS Y SUBMODULOS EN VARIABLES FIJAS, TAMBIEN SE OPTIMIZO LA LOGICA DE ENRUTAMIENTO Y SE COMENZO CON EL FORMULARIO PARA CREAR USUARIO

--- src/config/api/constants/apiRoutes.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'http://localhost:8090/tienda/api/v1';
-
-const API_ROUTES = {
-    LOGIN: '/auth/login',
-    PROFILE: '/auth/profile',
-    GET_USER_BY_UUID: '/usuario/uuid/'
-};
-
-export { BASE_URL, API_ROUTES }
